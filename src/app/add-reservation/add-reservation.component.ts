f5db935d6041d34b21931e973ab0a2a943f663e4
Stanisław Franczyk <sfranczyk5@gmail.com>
2021-03-29 20:06:27 +0200

    learning angular materials

---
+++ src/app/add-reservation/add-reservation.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+
+
+@Component({
+  templateUrl: './add-reservation.component.html',
+  styleUrls: ['./add-reservation.component.css']
+})
+export class AddReservationComponent implements OnInit {
+
+  
+  constructor() { }
+  
+  ngOnInit(): void {
+    
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,25 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {ReservationsComponent } from'./reservations/reservations.component';
 import { RouterModule } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReservationsComponent } from'./reservations/reservations.component';
+import { AddReservationComponent } from './add-reservation/add-reservation.component';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import {MatChipsModule} from '@angular/material/chips';
+import {MatSliderModule} from '@angular/material/slider';
+import {MatNativeDateModule} from '@angular/material/core';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ReservationsComponent
+    ReservationsComponent,
+    AddReservationComponent
   ],
   imports: [
     BrowserModule,
@@ -18,7 +30,21 @@ import { RouterModule } from '@angular/router';
     AppRoutingModule,
     RouterModule.forRoot([
       { path: 'reservations', component: ReservationsComponent},
-    ])
+      { path: 'add-reservation', component: AddReservationComponent},
+    ]),
+    BrowserAnimationsModule,
+
+    // Material Components
+    MatButtonModule,
+    MatCardModule,
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatChipsModule,
+    MatSliderModule,
+    MatNativeDateModule
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
