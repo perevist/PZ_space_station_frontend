9610cea3ef7560ec0c7fced2d16cdc5a529c14dd
Dawkon99 <Dawkon99@gmail.com>
2021-03-31 15:34:04 +0200

    Done new resrvation page

--- src/app/add-reservation/add-reservation.component.ts
+++ src/app/add-reservation/add-reservation.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface ReservatedWorkSite{
+  room: number;
+  workSite: number;
+}
 
 
 @Component({
@@ -7,6 +11,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-reservation.component.css']
 })
 export class AddReservationComponent implements OnInit {
+  reservatedWorkSites: ReservatedWorkSite[] = [];
+  visibleWorkSitesChips = true;
+  selectableWorkSitesChips = true;
+  removableWorkSitesChips = true;
+  addOnBlurWorkSitesChips = true;
 
   rooms: number[] = [1, 2, 3, 4, 5]; //TODO Zmienić przykład na interface z bazy danych
   workSitesIds: number[] = [1, 2, 3, 4, 5] //TODO Zmienić przykład na interface z bazy danych
@@ -14,6 +23,22 @@ export class AddReservationComponent implements OnInit {
   
   ngOnInit(): void {
     
+  }
+
+  add(room:number, workSite:number):void{
+    this.reservatedWorkSites.push({room: room, workSite:workSite});
+    const index = this.workSitesIds.indexOf(workSite);
+    this.workSitesIds.splice(index, 1);
+  }
+
+  remove(workSite: ReservatedWorkSite): void{
+    const index = this.reservatedWorkSites.indexOf(workSite);
+
+    if (index >= 0){
+      this.reservatedWorkSites.splice(index, 1);
+      this.workSitesIds.push(workSite.workSite);
+      this.workSitesIds.sort();
+    }
   }
 
 }
--- src/app/login-input/login-input.component.ts
+++ src/app/login-input/login-input.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-input',
   templateUrl: './login-input.component.html',
   styleUrls: ['./login-input.component.css']
 })
-export class LoginInputComponent {
+export class LoginInputComponent{
+  passwordControl: FormGroup = new FormGroup({
+    password: new FormControl('', [Validators.required, Validators.min(3)])
+  })
   hide = true;
   constructor() { }
 
