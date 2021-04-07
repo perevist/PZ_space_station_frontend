813392032fcfb86387023dc6b3574eea0565e3c4
Stanisław Franczyk <sfranczyk5@gmail.com>
2021-04-07 13:41:30 +0200

    Clear code reservations. Reservation chips cannot be create with this same dates and workside

--- src/app/add-reservation/add-reservation.component.ts
+++ src/app/add-reservation/add-reservation.component.ts
@@ -1,18 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl, FormGroup, Validators} from "@angular/forms";
-import { Floors } from '../model/Floors';
-import { Room } from '../model/Room';
-import { UserResponseDto } from '../model/UserResponseDto';
-import {AuthenticationService} from "../service/authentication.service";
-import { FloorsService } from '../service/floors.service';
-import { UsersService } from '../service/users.service';
-import {RoomsService} from '../service/rooms.service';
-import { WorksitesService } from '../service/worksites.service';
-import { Worksite } from '../model/Worksite';
+import { FormControl, FormGroup } from "@angular/forms";
 import { DatePipe } from '@angular/common';
+import { UsersService } from '../service/users.service';
+import { FloorsService } from '../service/floors.service';
+import { RoomsService } from '../service/rooms.service';
+import { WorksitesService } from '../service/worksites.service';
+import { UserResponseDto } from '../model/UserResponseDto';
+import { Room } from '../model/Room';
+import { Worksite } from '../model/Worksite';
 import { ReservationsService } from '../service/reservations.service';
-import {ReservationRequest} from '../model/ReservationRequest';
-
+import { ReservationRequest } from '../model/ReservationRequest';
 
 export interface ReservatedWorkSite{
   owner:	UserResponseDto;
@@ -29,44 +26,44 @@ export interface ReservatedWorkSite{
 })
 
 export class AddReservationComponent implements OnInit {
-  constructor(private usersService: UsersService,
-              private floorsService: FloorsService,
-              private roomsService: RoomsService,
-              private worksiteService: WorksitesService,
-              private reservationService: ReservationsService,
-              private datepipe: DatePipe) { }
+  constructor(
+    private usersService: UsersService,
+    private floorsService: FloorsService,
+    private roomsService: RoomsService,
+    private worksiteService: WorksitesService,
+    private reservationService: ReservationsService,
+    private datepipe: DatePipe
+  ) {}
 
   dateRange = new FormGroup({
     start: new FormControl(),
     end: new FormControl()
   });
 
-  visibleWorkSitesChips = true;
-  selectableWorkSitesChips = true;
   removable = true;
+  visibleWorkSitesChips = true;
   addOnBlurWorkSitesChips = true;
-  floors: number[] = [];
+  selectableWorkSitesChips = true;
   usersList: UserResponseDto[] = [];
+  floors: number[] = [];
   roomsList: Room[] = [];
   worksitesList: Worksite[] = [];
-  
-  reservatedWorkSites: ReservatedWorkSite[] = [];
+  reservatedWorkSites: ReservatedWorkSite[] = [];  
   reservated: ReservatedWorkSite;
   reservation: ReservationRequest;
-
 
   ngOnInit(): void {
     this.getUsers();
     this.getFloors();
   }
 
-  getUsers(){
+  getUsers(): void{
     this.usersService.getUsers().subscribe(
       users => this.usersList = users
     );
   }
 
-  getFloors(){
+  getFloors(): void{
     this.floorsService.getFloors().subscribe(
       numberOfFloors => {
         for(var i=1; i <= numberOfFloors.numberOfFloors; i++){
@@ -76,11 +73,9 @@ export class AddReservationComponent implements OnInit {
     );
   }
 
-  getRooms(floors?: number, start?:Date, end?:Date){
+  getRooms(floors?: number, start?:Date, end?:Date): void{
     this.roomsService.getRooms(floors.toString(), start, end).subscribe(
-      rooms => {
-        this.roomsList = rooms;
-    }
+      rooms => this.roomsList = rooms
     );
   }
 
@@ -92,76 +87,90 @@ export class AddReservationComponent implements OnInit {
 
   postReservations(){
     for(var reservatedWorkSite of this.reservatedWorkSites){
-      this.reservation = {startDate:reservatedWorkSite.startDate,
-                          endDate:reservatedWorkSite.endDate,
-                          ownerId:reservatedWorkSite.owner.id,
-                          worksiteId:reservatedWorkSite.worksite.worksiteId};
-      console.log(this.reservation);
-      this.reservationService.postReservation(this.reservation)
-      .subscribe(msg => {
-        console.log(msg) // RservationResponse
-      }, error => {
-        console.log(error.message)
-      });
+      this.reservation = {
+        startDate:reservatedWorkSite.startDate,
+        endDate:reservatedWorkSite.endDate,
+        ownerId:reservatedWorkSite.owner.id,
+        worksiteId:reservatedWorkSite.worksite.worksiteId
+      };
+      this.reservationService.postReservation( this.reservation )
+        .subscribe(msg => {
+          console.log(msg) // RservationResponse
+        }, error => {
+          console.log(error.message)
+        });
     }
     this.reservatedWorkSites = [];
   }
 
-
-  add(owner:	UserResponseDto,
-      floorNumber: number,
-      roomName: string,
-      worksite: Worksite,
-      startDate: Date,
-      endDate: Date): void{
-
+  addChipToList(  
+    owner:	UserResponseDto,
+    floorNumber: number,
+    roomName: string,
+    worksite: Worksite,
+    startDate: Date,
+    endDate: Date
+  ): void{
     let start = this.datepipe.transform(startDate, 'yyyy-MM-dd');
     let end = this.datepipe.transform(endDate, 'yyyy-MM-dd');
 
-    if(!this.check(owner, floorNumber, roomName, worksite, start, end)){
+    if(!this.checkIfChipExist(worksite, start, end)){
       this.reservatedWorkSites.push({owner, floorNumber, roomName, worksite, startDate:start, endDate:end});
     }
   }
 
-  check(owner:	UserResponseDto,
-      floorNumber: number,
-      roomName: string,
-      worksite: Worksite,
-      startDate: string,
-      endDate: string){
-        for(let i of this.reservatedWorkSites){
-<<<<<<< HEAD
-          if(i.owner === owner && i.floorNumber === floorNumber && i.roomName === roomName &&
-            i.worksite === worksite && i.startDate === startDate && i.endDate === endDate){
-=======
-          if(i.floorNumber === floorNumber && i.roomName === roomName &&
-            i.worksiteId === worksiteId && i.startDate === startDate && i.endDate === endDate){
->>>>>>> fef7f279a81b0b8137e88c1ab8b94edc8ef46179
-              return true;
-            }
-        }
-        return false;
+  // Backend accepts many of the same bookings to the base. The method can be saved for backend testing.
+  // check(owner:	UserResponseDto,
+  //     floorNumber: number,
+  //     roomName: string,
+  //     worksite: Worksite,
+  //     startDate: string,
+  //     endDate: string){
+  //       for(let i of this.reservatedWorkSites){
+  //         if(i.owner === owner && i.floorNumber === floorNumber && i.roomName === roomName &&
+  //           i.worksite.worksiteId === worksite.worksiteId && i.startDate === startDate && i.endDate === endDate){
+  //             return true;
+  //           }
+  //       }
+  //       return false;
+  //     }  
+
+  checkIfChipExist( worksite: Worksite, startDate: string, endDate: string ) {
+    for(let i of this.reservatedWorkSites){
+      if(i.worksite.worksiteId === worksite.worksiteId && i.startDate === startDate && i.endDate === endDate){
+        return true;
       }
+    }
+    return false;
+  }
 
-
-  checkIndex(owner:	UserResponseDto,
+  checkIndexChipInList( 
+    owner:	UserResponseDto,
     floorNumber: number,
     roomName: string,
     worksite: Worksite,
     startDate: string,
-    endDate: string){
-      let k = 0;
-      for(let i of this.reservatedWorkSites){
-        if(i.owner === owner && i.floorNumber === floorNumber && i.roomName === roomName &&
-          i.worksite === worksite && i.startDate === startDate && i.endDate === endDate){
-            return k;
-          }
-          k++;
-      }
+    endDate: string 
+  ){
+    let k = 0;
+    for(let i of this.reservatedWorkSites){
+      if(i.owner === owner && i.floorNumber === floorNumber && i.roomName === roomName &&
+        i.worksite === worksite && i.startDate === startDate && i.endDate === endDate){
+          return k;
+        }
+        k++;
     }
+  }
 
-  remove(reservated: ReservatedWorkSite): void{
-    let index = this.checkIndex(reservated.owner, reservated.floorNumber, reservated.roomName, reservated.worksite, reservated.startDate, reservated.endDate);
+  removeChipFormList(reservated: ReservatedWorkSite): void{
+    let index = this.checkIndexChipInList(
+      reservated.owner, 
+      reservated.floorNumber, 
+      reservated.roomName, 
+      reservated.worksite, 
+      reservated.startDate, 
+      reservated.endDate 
+    );
     if (index >= 0){
       this.reservatedWorkSites.splice(index, 1);
     }
