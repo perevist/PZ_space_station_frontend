d93a4ba0b32c8b65417244cb8677c6dcf047563d
Stanisław Franczyk <sfranczyk5@gmail.com>
2021-04-12 21:14:45 +0200

    add new component add-component

--- src/app/room/room.module.ts
+++ src/app/room/room.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AddRoomComponent } from './component/add-room/add-room.component';
+import { RouterModule } from '@angular/router';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [AddRoomComponent],
   imports: [
-    CommonModule
+    CommonModule,
+
+    RouterModule.forRoot([
+      { path: 'reservations-table', component: ReservationsTableComponent },
+      { path: 'add-reservation', component: AddReservationComponent }
+    ]),
+
   ]
 })
 export class RoomModule { }
